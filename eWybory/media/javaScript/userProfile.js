f393b0451b55139c428eb9b3b125b1ebeede4f5b
WPiotr <p.wyszkowski92@gmail.com>
2014-05-18 21:44:09 +0200

    dwa slesze

--- eWybory/media/javaScript/userProfile.js
+++ eWybory/media/javaScript/userProfile.js
@@ -2,13 +2,21 @@ $(document).ready(function(){
 	$('#emailBar').hide();
 	$('#passBar').hide();	
 	
-	$( "#changePass" ).click(function() {
+	$( '#changePass' ).click(function() {
  		$('#passBar').fadeToggle();	
 	});
 	
-	$( "#changeEmail" ).click(function() {
+	$( '#changeEmail' ).click(function() {
  		$('#emailBar').fadeToggle();	
 	});
+	$('#redirect').load('aaaaaaa',function(responseTxt,statusTxt,xhr){
+		if(statusTxt=="success")
+      		alert("External content loaded successfully!");	
+		if(statusTxt=="error")
+      		window.location = '/';
+	});
+	
+	
 	
 });	
 
